5b64aa4c8e15343eaf0f4562ddd0af8710a65e25
James Newton-King <james@newtonking.com>
2022-10-12 02:50:15 +0800

    Fix tests

--- src/Servers/Kestrel/Transport.NamedPipes/test/WebHostTests.cs
+++ src/Servers/Kestrel/Transport.NamedPipes/test/WebHostTests.cs
@@ -74,7 +74,8 @@ public class WebHostTests : LoggedTest
         }
     }
 
-    [Fact]
+    [ConditionalFact]
+    [OSSkipCondition(OperatingSystems.Linux | OperatingSystems.MacOSX, SkipReason = "Impersonation is only supported on Windows.")]
     public async Task ListenNamedPipeEndpoint_Impersonation_ClientSuccess()
     {
         AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
@@ -222,7 +223,8 @@ public class WebHostTests : LoggedTest
         };
     }
 
-    [Theory]
+    [ConditionalTheory]
+    [OSSkipCondition(OperatingSystems.MacOSX, SkipReason = "Missing SslStream ALPN support: https://github.com/dotnet/runtime/issues/27727")]
     [InlineData(HttpProtocols.Http1)]
     [InlineData(HttpProtocols.Http2)]
     public async Task ListenNamedPipeEndpoint_Tls_ClientSuccess(HttpProtocols protocols)

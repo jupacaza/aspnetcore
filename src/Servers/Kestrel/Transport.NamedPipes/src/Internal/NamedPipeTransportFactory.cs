a1ba47c6bbd45bd4e09d923cf14ed61e9e43699a
James Newton-King <james@newtonking.com>
2022-10-11 16:29:26 +0800

    Fix build

--- src/Servers/Kestrel/Transport.NamedPipes/src/Internal/NamedPipeTransportFactory.cs
+++ src/Servers/Kestrel/Transport.NamedPipes/src/Internal/NamedPipeTransportFactory.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
-using System.Threading;
 using Microsoft.AspNetCore.Connections;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
